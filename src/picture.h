a904f4b5ada4fadd53548b38a548c3eb9b34a0de
JacksonPaulHeck <jacksonheck1@gmail.com>
2021-08-26 13:36:00 -0500

    Create picture.h and picture.cpp to implement the Picture Class. This resolves the Create Picture Issue.

---
+++ src/picture.h
@@ -0,0 +1,26 @@
+struct Coordinate {
+    int x, y;
+};
+
+class Picture {
+    private:
+        int id;
+        int price;
+        int width;
+        int height;
+        Coordinate coordinates;
+
+    public:
+        Picture();
+        Picture(int, int, int, int);
+        int getID();
+        int getPrice();
+        int getWidth();
+        int getHeight();
+        Coordinate getCoordinates();
+        void setID(int);
+        void setPrice(int);
+        void setWidth(int);
+        void setHeight(int);
+        void setCoordinates(int, int);
+};
